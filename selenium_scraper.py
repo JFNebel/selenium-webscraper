da04f07a28020d084a7007381ef58844df534313
JFNebel <jfnebeldunn@gmail.com>
2022-02-07 00:46:12 -0500

    Finished with requirements

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -10,16 +10,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
 
-
-# Logs configuration
-logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("logs/selenium_scraper.log", mode="w")
-formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-logger.setLevel(logging.DEBUG)
-
-
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('myLogger')
 
 def main():
 
@@ -71,13 +63,14 @@ def input_search(driver: WebDriver):
 
 def press_button(driver: WebDriver):
 
+    # TODO: You could do an explicit wait here, instead of this.
     sleep(random.uniform(4.0, 6.0)) # Button sometimes takes time to load
 
     for i in range(4):
 
         load_content_button = driver.find_element(By.XPATH, const.LOAD_CONTENT_BUTTON_XPATH)
         load_content_button.click()
-        sleep(random.uniform(8.0, 12.0)) # Makes it more human-like
+        sleep(random.uniform(4.0, 7.0)) # Makes it more human-like
 
         logger.info(f"Loaded more content by pressing the button successfully ({i+1}).")
 
